694372e19511e1b33b7622b1d2e466a95381242f
Julie Poulain <juliempoulain@gmail.com>
2024-03-13 16:48:12 -0700

    added event listener for submit, prevent default, append tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newTask = e.target["new-task-description"].value;
+    console.log(newTask);
+    const li = document.createElement("li");
+    document.querySelector("#tasks").append(li);
+    li.textContent = `${newTask}`;
+  });
 });
